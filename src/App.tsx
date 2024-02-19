2949918d7f7b5518616fe4036bc5eb569907d684
rukavyshnykov <82444499+rukavyshnykov@users.noreply.github.com>
2024-02-19 21:23:44 +0200

    nvm nothing really useful

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import { useState } from 'react'
 
+import { Input } from './components/ui/Input/Input'
 import { SuperSelect } from './components/ui/Select'
 
 export function App() {
-  const mockOptions = [
-    { label: 'opt1', value: 'opt1' },
-    { label: 'opt2', value: 'opt2' },
-    { label: 'opt3', value: 'opt3' },
-    { label: 'opt4', value: 'opt4' },
-    { label: 'opt5', value: 'opt5' },
-  ]
-
-  const [state, setState] = useState(mockOptions[0].value)
-  const handleSelectChange = (value: string) => {
-    setState(value)
-  }
+  const [value, setValue] = useState('')
 
   return (
     <>
       <div>Hello</div>
-      <SuperSelect handleSelectChange={handleSelectChange} options={mockOptions} value={state} />
+      <Input
+        label={'check'}
+        onChange={e => setValue(e.target.value)}
+        placeholder={'just checking'}
+        type={'password'}
+        value={value}
+      />
     </>
   )
 }
