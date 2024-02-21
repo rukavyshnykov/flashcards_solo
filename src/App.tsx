08ff691902f853d8bcbf5427367db86a5d08833a
rukavyshnykov <82444499+rukavyshnykov@users.noreply.github.com>
2024-02-21 20:51:20 +0200

    update: added icons to buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { CreateNewPasswordForm } from './components/forms/CreateNewPassword'
+import { Button } from './components/ui/Button'
 
 export function App() {
   return (
     <>
       <div>Hello</div>
       <CreateNewPasswordForm />
+      <Button iconId={'edit'}>dfaskdjfhskjd</Button>
     </>
   )
 }
--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -2,21 +2,35 @@ import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
 
 import c from './Button.module.scss'
 
+import { Icon } from '../Icon/Icon'
+
 export type ButtonProps<T extends ElementType = 'button'> = {
   as?: T
-  children: ReactNode
+  children?: ReactNode
   className?: string
   fullWidth?: boolean
+  iconId?: string
   variant?: 'primary' | 'secondary'
 } & ComponentPropsWithoutRef<T>
 
 export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) => {
-  const { as: Component = 'button', className, fullWidth, variant = 'primary', ...rest } = props
+  const {
+    as: Component = 'button',
+    children,
+    className,
+    fullWidth,
+    iconId,
+    variant = 'primary',
+    ...rest
+  } = props
 
   return (
     <Component
       className={c.button + ` ${c[variant]} ${fullWidth ? c.fullWidth : ''} ${className}`}
       {...rest}
-    />
+    >
+      {iconId && <Icon fill={'white'} height={16} iconId={iconId} width={16} />}
+      {children}
+    </Component>
   )
 }
