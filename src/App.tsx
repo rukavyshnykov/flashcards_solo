a1ba8b090976af4592cb7ac9422e078ab85ac3b0
rukavyshnykov <82444499+rukavyshnykov@users.noreply.github.com>
2024-02-22 17:43:23 +0200

    update: installed radix radio

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { ForgotPasswordForm } from './components/forms/ForgotPassword'
 import { SignUpForm } from './components/forms/SignUp'
+import { Radio } from './components/ui/Radio'
 
 export function App() {
   return (
     <>
       <div>Hello</div>
-      <SignUpForm />
+      <Radio />
     </>
   )
 }
