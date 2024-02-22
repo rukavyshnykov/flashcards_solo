7210322eabbd8cd8c159f31cf0b32f6f102959c2
rukavyshnykov <82444499+rukavyshnykov@users.noreply.github.com>
2024-02-22 10:43:10 +0200

    update: forgot password added second stage

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
-import { useState } from 'react'
-
-import { CreateNewPasswordForm } from './components/forms/CreateNewPassword'
-import { EditProfileForm } from './components/forms/EditProfile'
-import { Button } from './components/ui/Button'
-import { FileUploader } from './components/ui/FileUploader/FileUploader'
+import { ForgotPasswordForm } from './components/forms/ForgotPassword'
 
 export function App() {
-  const [src, setSrc] = useState<string>('')
-
-  const setImageSrc = (source: string) => {
-    setSrc(source)
-  }
-
   return (
     <>
       <div>Hello</div>
-      <CreateNewPasswordForm />
-      <Button iconId={'edit'} variant={'secondary'}>
-        dfaskdjfhskjd
-      </Button>
-      <img src={src} />
-      <FileUploader fullWidth={false} iconId={'edit'} setImageSrc={setImageSrc} />
-      <EditProfileForm />
+      <ForgotPasswordForm />
     </>
   )
 }
--- src/components/forms/ForgotPassword/ForgotPassword.tsx
+++ src/components/forms/ForgotPassword/ForgotPassword.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 
+import checkEmail from '@/assets/check-email.png'
 import { ControlledInput } from '@/components/controlled/ControlledInput'
 import { Button } from '@/components/ui/Button'
 import { Card } from '@/components/ui/Card'
@@ -13,39 +15,58 @@ type FormValues = {
 
 export const ForgotPasswordForm = () => {
   const { control, handleSubmit } = useForm<FormValues>()
+  const [isSuccess, setIsSuccess] = useState<boolean>(false)
 
   const onSubmit = (data: any) => {
+    setIsSuccess(true)
     console.log(data)
   }
 
   return (
     <Card>
-      <Typography variant={'h1'}>Forgot your password?</Typography>
-      <form className={c.form} onSubmit={handleSubmit(onSubmit)}>
-        <div className={c.inputsWrapper}>
-          <ControlledInput
-            control={control}
-            defaultValue={''}
-            label={'Email'}
-            name={'email'}
-            type={'text'}
-          />
-          <Typography className={c.caption} variant={'body2'}>
-            Enter your email address and we will send you further instructions{' '}
-          </Typography>
-        </div>
-        <Button fullWidth type={'submit'}>
-          Send Instructions
-        </Button>
-        <div className={c.formFooter}>
-          <Typography className={c.caption} variant={'body2'}>
-            Did you remember your password?
-          </Typography>
-          <Typography className={c.signUp} variant={'body1'}>
-            Try logging in
-          </Typography>
-        </div>
-      </form>
+      {!isSuccess ? (
+        <>
+          <Typography variant={'h1'}>Forgot your password?</Typography>
+          <form className={c.form} onSubmit={handleSubmit(onSubmit)}>
+            <div className={c.inputsWrapper}>
+              <ControlledInput
+                control={control}
+                defaultValue={''}
+                label={'Email'}
+                name={'email'}
+                type={'text'}
+              />
+              <Typography className={c.caption} variant={'body2'}>
+                Enter your email address and we will send you further instructions
+              </Typography>
+            </div>
+            <Button fullWidth type={'submit'}>
+              Send Instructions
+            </Button>
+            <div className={c.formFooter}>
+              <Typography className={c.caption} variant={'body2'}>
+                Did you remember your password?
+              </Typography>
+              <Typography className={c.signUp} variant={'body1'}>
+                Try logging in
+              </Typography>
+            </div>
+          </form>
+        </>
+      ) : (
+        <>
+          <Typography variant={'h1'}>Check Email</Typography>
+          <div className={c.content}>
+            <img src={checkEmail} />
+            <Typography className={c.caption} variant={'body2'}>
+              We've sent an Email with instructions to example@mail.com
+            </Typography>
+          </div>
+          <Button fullWidth>
+            <Typography variant={'subtitle2'}>Send Instructions</Typography>
+          </Button>
+        </>
+      )}
     </Card>
   )
 }
