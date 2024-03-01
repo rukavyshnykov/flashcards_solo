9ad350e113d0a8a378a498fab93bfbf4a849fa9e
rukavyshnykov <82444499+rukavyshnykov@users.noreply.github.com>
2024-03-01 22:18:34 +0200

    update: added dropdown icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { LoginForm } from './components/forms/Auth'
+import { EditProfileForm } from './components/forms/EditProfile'
+import { Dropdown } from './components/ui/Dropdown/Dropdown'
+import { Input } from './components/ui/Input'
 
 export function App() {
   return (
     <>
-      <div>Hello</div>
+      <Dropdown>sdfsfsdf</Dropdown>
+      <EditProfileForm />
       <LoginForm />
     </>
   )
