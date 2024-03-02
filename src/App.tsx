dba2828a6aac171f1265051fa029b09cd8f2ea67
rukavyshnykov <82444499+rukavyshnykov@users.noreply.github.com>
2024-03-02 10:25:41 +0200

    update: added fixed ver of dropdown menu icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { LoginForm } from './components/forms/Auth'
 import { EditProfileForm } from './components/forms/EditProfile'
-import { Dropdown } from './components/ui/Dropdown/Dropdown'
+import { Dropdown, DropdownItem } from './components/ui/Dropdown/Dropdown'
 import { Input } from './components/ui/Input'
 
 export function App() {
   return (
     <>
-      <Dropdown>sdfsfsdf</Dropdown>
+      <Dropdown>
+        <DropdownItem>check</DropdownItem>
+        <DropdownItem>checkOne</DropdownItem>
+        <DropdownItem>checkOneTWO</DropdownItem>
+      </Dropdown>
       <EditProfileForm />
       <LoginForm />
     </>
