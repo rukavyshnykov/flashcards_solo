e02e5b73a45e550ffbff4593a3f408ce1f090bd9
rukavyshnykov <82444499+rukavyshnykov@users.noreply.github.com>
2024-03-02 10:26:13 +0200

    dropdown menu layout fix

--- src/components/ui/Dropdown/Dropdown.tsx
+++ src/components/ui/Dropdown/Dropdown.tsx
@@ -2,23 +2,36 @@ import { ComponentPropsWithoutRef, ReactNode } from 'react'
 
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 
+import c from './Dropdown.module.scss'
+
 import { Icon } from '../Icon/Icon'
 
 export const Dropdown = ({ children, trigger }: DropdownProps) => {
   return (
     <DropdownMenu.Root>
-      <DropdownMenu.Trigger>
-        {trigger ?? <Icon height={20} iconId={'dropdown'} width={20} />}
+      <DropdownMenu.Trigger className={c.trigger}>
+        {trigger ?? <Icon height={24} iconId={'dropdown'} width={24} />}
       </DropdownMenu.Trigger>
       <DropdownMenu.Portal>
-        <DropdownMenu.Content>{children}</DropdownMenu.Content>
+        <DropdownMenu.Content align={'end'} alignOffset={-11} className={c.content} side={'bottom'}>
+          {children}
+          <DropdownMenu.Arrow className={c.arrow1} height={8} width={16} />
+          <DropdownMenu.Arrow className={c.arrow2} height={8} width={16} />
+        </DropdownMenu.Content>
       </DropdownMenu.Portal>
     </DropdownMenu.Root>
   )
 }
 
-export const DropdownItem = ({ children, ...rest }: DropdownItemProps) => {
-  return <DropdownMenu.Item {...rest}>{children}</DropdownMenu.Item>
+export const DropdownItem = ({ children, className, ...rest }: DropdownItemProps) => {
+  return (
+    <>
+      <DropdownMenu.Item className={c.item + ` ${className}`} {...rest}>
+        {children}
+      </DropdownMenu.Item>
+      <DropdownMenu.Separator className={c.separator} />
+    </>
+  )
 }
 
 type DropdownProps = {
