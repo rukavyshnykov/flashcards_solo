57cdd04ff5be21fa4f1bf60d46211bd1891542ca
rukavyshnykov <82444499+rukavyshnykov@users.noreply.github.com>
2024-03-02 12:50:25 +0200

    update: fixed dropdown appearance

--- src/components/ui/Dropdown/Dropdown.tsx
+++ src/components/ui/Dropdown/Dropdown.tsx
@@ -13,7 +13,13 @@ export const Dropdown = ({ children, trigger }: DropdownProps) => {
         {trigger ?? <Icon height={24} iconId={'dropdown'} width={24} />}
       </DropdownMenu.Trigger>
       <DropdownMenu.Portal>
-        <DropdownMenu.Content align={'end'} alignOffset={-11} className={c.content} side={'bottom'}>
+        <DropdownMenu.Content
+          align={'end'}
+          alignOffset={-11}
+          className={c.content}
+          side={'bottom'}
+          sideOffset={2}
+        >
           {children}
           <DropdownMenu.Arrow className={c.arrow1} height={8} width={16} />
           <DropdownMenu.Arrow className={c.arrow2} height={8} width={16} />
