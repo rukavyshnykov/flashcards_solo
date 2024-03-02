24e864bfa251fdd7305af40e4b77edd9ccd2966a
rukavyshnykov <82444499+rukavyshnykov@users.noreply.github.com>
2024-03-02 12:49:31 +0200

    update: added logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { LoginForm } from './components/forms/Auth'
 import { EditProfileForm } from './components/forms/EditProfile'
 import { Dropdown, DropdownItem } from './components/ui/Dropdown/Dropdown'
+import { Header } from './components/ui/Header/Header'
 import { Input } from './components/ui/Input'
 
 export function App() {
   return (
     <>
+      <Header />
       <Dropdown>
         <DropdownItem>check</DropdownItem>
         <DropdownItem>checkOne</DropdownItem>
