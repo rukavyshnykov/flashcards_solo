4d5986bcb6d36575065837f2dac0cee692139840
rukavyshnykov <82444499+rukavyshnykov@users.noreply.github.com>
2024-03-08 17:57:02 +0200

    update: router
    created PrivateRoutes component, router rebuild

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,16 @@
-import { RouteObject, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import {
+  Navigate,
+  Outlet,
+  RouteObject,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom'
 
 import { DecksPage } from './components/pages/Decks/Decks'
 import { ForgotPasswordPage } from './components/pages/ForgotPassword/ForgotPassword'
 import { Layout } from './components/pages/Layout/Layout'
 import { LoginPage } from './components/pages/Login/Login'
+import { useGetMeQuery } from './components/pages/Login/authApi'
 import { RegisterPage } from './components/pages/Register/Register'
 
 const publicRoutes: RouteObject[] = [
@@ -28,9 +35,25 @@ const privateRoutes: RouteObject[] = [
   },
 ]
 
-const router = createBrowserRouter([
+const PrivateRoutes = () => {
+  const { isError, isLoading } = useGetMeQuery()
+
+  if (isLoading) {
+    return <>Loader...</>
+  }
+
+  return isError ? <Navigate to={'/login'} /> : <Outlet />
+}
+
+export const router = createBrowserRouter([
   {
-    children: [...publicRoutes, ...privateRoutes],
+    children: [
+      ...publicRoutes,
+      {
+        children: privateRoutes,
+        element: <PrivateRoutes />,
+      },
+    ],
     element: <Layout />,
   },
 ])
