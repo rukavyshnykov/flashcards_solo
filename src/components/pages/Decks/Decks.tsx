8f819f12a33c42f6fa68472da688eeeb7791cc16
rukavyshnykov <82444499+rukavyshnykov@users.noreply.github.com>
2024-03-09 23:33:12 +0200

    update: added bl to decks page

--- src/components/pages/Decks/Decks.tsx
+++ src/components/pages/Decks/Decks.tsx
@@ -1,24 +1,53 @@
+import { useState } from 'react'
+
+import { PaginationBar } from '@/components/ui/Pagination/Pagination'
 import { Table } from '@/components/ui/Table'
 
+import { useGetDecksQuery } from './decksApi'
+
 export const DecksPage = () => {
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const [itemsPerPage, setItemsPerPage] = useState<number>(5)
+  const { data } = useGetDecksQuery({
+    authorId: '',
+    currentPage: currentPage,
+    itemsPerPage: itemsPerPage,
+    maxCardsCount: 30,
+    minCardsCount: 10,
+    name: '',
+    orderBy: null,
+  })
+
+  const setPage = (page: number) => {
+    setCurrentPage(page)
+  }
+  const setItems = (items: number) => {
+    setItemsPerPage(items)
+  }
+
   return (
-    <Table.Root>
-      <Table.Head>
-        <Table.Row>
-          <Table.HeadCell>Name</Table.HeadCell>
-          <Table.HeadCell>Cards</Table.HeadCell>
-          <Table.HeadCell>Last Updated</Table.HeadCell>
-          <Table.HeadCell>Created By</Table.HeadCell>
-        </Table.Row>
-      </Table.Head>
-      <Table.Body>
-        <Table.Row>
-          <Table.Cell>asdfdsfs</Table.Cell>
-          <Table.Cell>2</Table.Cell>
-          <Table.Cell>12</Table.Cell>
-          <Table.Cell>afsdfds</Table.Cell>
-        </Table.Row>
-      </Table.Body>
-    </Table.Root>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <Table.Root>
+        <Table.Head>
+          <Table.Row>
+            <Table.HeadCell>Name</Table.HeadCell>
+            <Table.HeadCell>Cards</Table.HeadCell>
+            <Table.HeadCell>Last Updated</Table.HeadCell>
+            <Table.HeadCell>Created By</Table.HeadCell>
+          </Table.Row>
+        </Table.Head>
+        <Table.Body>
+          {data?.items.map(item => (
+            <Table.Row key={item.id}>
+              <Table.Cell>{item.name}</Table.Cell>
+              <Table.Cell>{item.cardsCount}</Table.Cell>
+              <Table.Cell>{item.updated}</Table.Cell>
+              <Table.Cell>{item.author.name}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+      <PaginationBar setItems={setItems} setPage={setPage} />
+    </div>
   )
 }
