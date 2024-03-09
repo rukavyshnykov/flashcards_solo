a48c450ab3b3629ee18ded36f9443239946edbd2
rukavyshnykov <82444499+rukavyshnykov@users.noreply.github.com>
2024-03-09 12:09:09 +0200

    update: added business logic to register page

--- src/components/pages/Register/Register.tsx
+++ src/components/pages/Register/Register.tsx
@@ -1,5 +1,24 @@
-import { SignUpForm } from '@/components/forms/SignUp'
+import { useNavigate } from 'react-router-dom'
+
+import { RegisterFormValues, SignUpForm } from '@/components/forms/SignUp'
+
+import { useLoginMutation, useRegisterMutation } from '../Login/authApi'
 
 export const RegisterPage = () => {
-  return <SignUpForm />
+  const [register] = useRegisterMutation()
+  const [login] = useLoginMutation()
+
+  const navigate = useNavigate()
+
+  const onSubmit = async ({ email, password }: RegisterFormValues) => {
+    register({ email, password })
+      .unwrap()
+      .then(() => {
+        login({ email, password })
+          .unwrap()
+          .then(() => navigate('/'))
+      })
+  }
+
+  return <SignUpForm onSubmit={onSubmit} />
 }
