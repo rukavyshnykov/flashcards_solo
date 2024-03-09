da7b1b16a696c988e2f7f97deb09c910967bc0d6
rukavyshnykov <82444499+rukavyshnykov@users.noreply.github.com>
2024-03-09 12:12:20 +0200

    feature: added register and seMe endpoints

--- src/components/pages/Login/authApi.ts
+++ src/components/pages/Login/authApi.ts
@@ -1,7 +1,7 @@
 import { LoginFormValues } from '@/components/forms/Auth'
 import { baseApi } from '@/services/baseApi'
 
-import { User } from './authTypes'
+import { RegisterArgs, User } from './authTypes'
 
 const authApi = baseApi.injectEndpoints({
   endpoints: builder => ({
@@ -27,7 +27,37 @@ const authApi = baseApi.injectEndpoints({
         url: '/v1/auth/logout',
       }),
     }),
+    register: builder.mutation<User, RegisterArgs>({
+      invalidatesTags: ['User'],
+      query: body => ({
+        body,
+        method: 'POST',
+        url: '/v1/auth/sign-up',
+      }),
+    }),
+    setMe: builder.mutation<User, { avatar?: File | null; name?: string }>({
+      invalidatesTags: ['User'],
+      query: body => {
+        const formData = new FormData()
+
+        body.avatar && formData.append('avatar', body.avatar)
+
+        body.name && formData.append('name', body.name)
+
+        return {
+          body: formData,
+          method: 'PATCH',
+          url: '/v1/auth/me',
+        }
+      },
+    }),
   }),
 })
 
-export const { useGetMeQuery, useLoginMutation, useLogoutMutation } = authApi
+export const {
+  useGetMeQuery,
+  useLoginMutation,
+  useLogoutMutation,
+  useRegisterMutation,
+  useSetMeMutation,
+} = authApi
