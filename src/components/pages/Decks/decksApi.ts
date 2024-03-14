5ec57ed7098d941e6fa801939840c53be6d7f13d
rukavyshnykov <82444499+rukavyshnykov@users.noreply.github.com>
2024-03-14 23:25:27 +0200

    update: added minmax endpoint

--- src/components/pages/Decks/decksApi.ts
+++ src/components/pages/Decks/decksApi.ts
@@ -1,18 +1,23 @@
 import { baseApi } from '@/services/baseApi'
 
-import { DecksArgs, DecksResponse } from './decksTypes'
+import { DecksArgs, DecksResponse, MinMax } from './decksTypes'
 
 const decksApi = baseApi.injectEndpoints({
   endpoints: builder => ({
     getDecks: builder.query<DecksResponse, DecksArgs>({
       providesTags: ['Decks'],
-      query: body => ({
-        method: 'GET',
-        params: body,
+      query: params => ({
+        params,
         url: '/v2/decks',
+      }),
+    }),
+    getDecksMinMax: builder.query<MinMax, void>({
+      providesTags: ['Decks'],
+      query: () => ({
+        url: '/v2/decks/min-max-cards',
       }),
     }),
   }),
 })
 
-export const { useGetDecksQuery } = decksApi
+export const { useGetDecksMinMaxQuery, useGetDecksQuery } = decksApi
