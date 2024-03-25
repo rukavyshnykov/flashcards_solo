a885ed8385d43e3c7347a2f6fba9c9795194fecb
rukavyshnykov <82444499+rukavyshnykov@users.noreply.github.com>
2024-03-25 15:01:46 +0200

    added new endpoints

--- src/components/pages/Decks/decksApi.ts
+++ src/components/pages/Decks/decksApi.ts
@@ -1,18 +1,56 @@
 import { baseApi } from '@/services/baseApi'
 
-import { DecksArgs, DecksResponse, MinMax } from './decksTypes'
+import { CreateDeckArgs, Deck, DecksArgs, DecksResponse, MinMax } from './decksTypes'
 
 const decksApi = baseApi.injectEndpoints({
   endpoints: builder => ({
-    getDecks: builder.query<DecksResponse, DecksArgs>({
-      providesTags: ['Decks'],
+    addDeck: builder.mutation<Deck, CreateDeckArgs>({
+      invalidatesTags: ['Decks'],
+      query: body => {
+        const formData = new FormData()
+
+        body.cover && formData.append('cover', body.cover)
+        formData.append('name', body.name)
+        formData.append('isPrivate', String(body.isPrivate))
+
+        return {
+          body: formData,
+          method: 'POST',
+          url: '/v1/decks',
+        }
+      },
+    }),
+    deleteDeck: builder.mutation<Deck, { id: string }>({
+      invalidatesTags: ['Decks'],
+      query: body => ({
+        method: 'DELETE',
+        url: `/v1/decks/${body.id}`,
+      }),
+    }),
+    getCards: builder.query<any, { id: string }>({
+      // providesTags: ['Deck'],
+      query: ({ id }) => ({
+        url: `/v1/decks/${id}/cards`,
+      }),
+    }),
+    getDeck: builder.query<any, { id: string }>({
+      providesTags: arg => [{ id: arg.id, type: 'Deck' }],
+      query: ({ id }) => ({
+        url: `/v1/decks/${id}`,
+      }),
+    }),
+    getDecks: builder.query<DecksResponse, DecksArgs | undefined>({
+      providesTags: result =>
+        result
+          ? [...result.items.map(({ id }) => ({ id, type: 'Deck' as const })), 'Decks']
+          : ['Decks'],
       query: params => ({
         params,
         url: '/v2/decks',
       }),
     }),
     getDecksMinMax: builder.query<MinMax, void>({
-      providesTags: ['Decks'],
+      // providesTags: ['Decks'],
       query: () => ({
         url: '/v2/decks/min-max-cards',
       }),
@@ -20,4 +58,11 @@ const decksApi = baseApi.injectEndpoints({
   }),
 })
 
-export const { useGetDecksMinMaxQuery, useGetDecksQuery } = decksApi
+export const {
+  useAddDeckMutation,
+  useDeleteDeckMutation,
+  useGetCardsQuery,
+  useGetDeckQuery,
+  useGetDecksMinMaxQuery,
+  useGetDecksQuery,
+} = decksApi
