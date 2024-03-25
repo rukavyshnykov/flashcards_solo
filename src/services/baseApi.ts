ca3fdd0d684911993c575f2e1c17b388a4bd7019
rukavyshnykov <82444499+rukavyshnykov@users.noreply.github.com>
2024-03-25 15:03:29 +0200

    added new tag to base api

--- src/services/baseApi.ts
+++ src/services/baseApi.ts
@@ -6,5 +6,5 @@ export const baseApi = createApi({
   baseQuery: baseQueryWithReauth,
   endpoints: () => ({}),
   reducerPath: 'baseApi',
-  tagTypes: ['User', 'Decks'],
+  tagTypes: ['User', 'Decks', 'Deck'],
 })
